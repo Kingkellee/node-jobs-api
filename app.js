dd0e923135ec6241e615778dc57c8b005e57c522
KingKellee <kellyiyogun@gmail.com>
2022-03-06 22:40:59 +0100

    set up register functionality

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const express = require('express');
 const app = express();
 
 // connectDB
+const connectDB = require('./db/connect');
 
 // router
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
-
-// Error Handler
+// error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -20,10 +20,15 @@ app.use(express.json());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', jobsRouter);
 
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 // server
+
 const port = process.env.PORT || 3000;
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => console.log(`server listening on PORT:${port}...`));
   } catch (error) {
     console.log(error);
---
+++ models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please input your Name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+
+  // -Validate email with mongoose
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: {
+      validator: function (value) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+      },
+      message: 'Please enter a valid email',
+    },
+    required: [true, 'Email is required'],
+  },
+
+  password: {
+    type: String,
+    required: [true, 'Please Provide a Password'],
+    minlength: 6,
+  },
+});
+
+// -Hash Password with bcryptjs
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// create token
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+module.exports = mongoose.model('User', UserSchema);
